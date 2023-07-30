19b45602ef34b1ca96c68d0fe94f60b529010078
Arker123 <kharbandarnav@gmail.com>
2023-07-31 03:54:31 +0530

    Tweaks

---
+++ main.py
@@ -0,0 +1,41 @@
+import textbase
+from textbase.message import Message
+from textbase import models
+import os
+from typing import List
+
+# Load your OpenAI API key
+models.OpenAI.api_key = "YOUR_API_KEY"
+# or from environment variable:
+# models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
+
+# Prompt for GPT-3.5 Turbo
+SYSTEM_PROMPT = """I want you to act as a mental health adviser. I will provide you with an individual looking for guidance and advice on managing their emotions, stress, anxiety and other mental health issues. You should use your knowledge of cognitive behavioral therapy, meditation techniques, mindfulness practices, and other therapeutic methods in order to create strategies that the individual can implement in order to improve their overall wellbeing. 
+ Never advise the individual to reach out to a mental health professional.
+ Write your responce in not more than 200 words.
+ Always be positive and encouraging.
+"""
+
+
+@textbase.chatbot("talking-bot")
+def on_message(message_history: List[Message], state: dict = None):
+    """Your chatbot logic here
+    message_history: List of user messages
+    state: A dictionary to store any stateful information
+
+    Return a string with the bot_response or a tuple of (bot_response: str, new_state: dict)
+    """
+
+    if state is None or "counter" not in state:
+        state = {"counter": 0}
+    else:
+        state["counter"] += 1
+
+    # # Generate GPT-3.5 Turbo response
+    bot_response = models.OpenAI.generate(
+        system_prompt=SYSTEM_PROMPT,
+        message_history=message_history,
+        model="gpt-3.5-turbo",
+    )
+
+    return bot_response, state
